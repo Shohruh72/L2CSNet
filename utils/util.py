46a932bc2e46481ccf1ebfcc0f5e000f7d73ef38
Shohruh <122860237+Shohruh72@users.noreply.github.com>
2024-05-03 16:27:05 +0900

    Update util.py

--- utils/util.py
+++ utils/util.py
@@ -1,6 +1,25 @@
-import math
 import torch
 import numpy as np
+
+
+class AverageMeter:
+    def __init__(self):
+        self.num = 0
+        self.sum = 0
+        self.avg = 0
+
+    def update(self, v, n):
+        self.num = self.num + n
+        self.sum = self.sum + v * n
+        self.avg = self.sum / self.num
+
+
+def strip_optimizer(filename):
+    x = torch.load(filename, map_location=torch.device('cpu'))
+    x['model'].half()  # to FP16
+    for p in x['model'].parameters():
+        p.requires_grad = False
+    torch.save(x, filename)
 
 
 def get_ignored_params(model):
@@ -34,12 +53,7 @@ def get_fc_params(model):
                 yield param
 
 
-def angular(gaze, label):
-    total = np.sum(gaze * label)
-    return np.arccos(min(total / (np.linalg.norm(gaze) * np.linalg.norm(label)), 0.9999999)) * 180 / np.pi
-
-
-def gaze_3d(gaze):
+def gazeto3d(gaze):
     gaze_gt = np.zeros([3])
     gaze_gt[0] = -np.cos(gaze[1]) * np.sin(gaze[0])
     gaze_gt[1] = -np.sin(gaze[1])
@@ -47,46 +61,22 @@ def gaze_3d(gaze):
     return gaze_gt
 
 
-def calculate_errors(gaze_predicted, label):
-    pitch_predicted, yaw_predicted = gaze_predicted
-    label_pitch, label_yaw = label
-
-    # Calculate pitch and yaw errors
-    pitch_error = angular(gaze_3d([pitch_predicted, 0]), gaze_3d([label_pitch, 0]))
-    yaw_error = angular(gaze_3d([0, yaw_predicted]), gaze_3d([0, label_yaw]))
-
-    return pitch_error, yaw_error
+def angular(gaze, label):
+    total = np.sum(gaze * label)
+    return np.arccos(min(total / (np.linalg.norm(gaze) * np.linalg.norm(label)), 0.9999999)) * 180 / np.pi
 
 
-def strip_optimizer(filename):
-    x = torch.load(filename, map_location=torch.device('cpu'))
-    x['model'].half()  # to FP16
-    for p in x['model'].parameters():
-        p.requires_grad = False
-    torch.save(x, filename)
-
-
-class AverageMeter:
-    def __init__(self):
-        self.num = 0
-        self.sum = 0
-        self.avg = 0
-
-    def update(self, v, n):
-        self.num = self.num + n
-        self.sum = self.sum + v * n
-        self.avg = self.sum / self.num
-
-
-class CosineLR:
-    def __init__(self, args, optimizer):
-        self.min_lr = 1E-6
-        self.epochs = args.epochs
-        self.learning_rates = [x['lr'] for x in optimizer.param_groups]
-
-    def step(self, epoch, optimizer):
-        param_groups = optimizer.param_groups
-        for param_group, lr in zip(param_groups, self.learning_rates):
-            alpha = math.cos(math.pi * epoch / self.epochs)
-            lr = 0.5 * (lr - self.min_lr) * (1 + alpha)
-            param_group['lr'] = self.min_lr + lr
+def draw_gaze(a, b, c, d, image_in, pitchyaw, thickness=2, color=(255, 255, 0)):
+    import cv2
+    image_out = image_in
+    (h, w) = image_in.shape[:2]
+    length = c
+    pos = (int(a + c / 2.0), int(b + d / 2.0))
+    if len(image_out.shape) == 2 or image_out.shape[2] == 1:
+        image_out = cv2.cvtColor(image_out, cv2.COLOR_GRAY2BGR)
+    dx = -length * np.sin(pitchyaw[0]) * np.cos(pitchyaw[1])
+    dy = -length * np.sin(pitchyaw[1])
+    cv2.arrowedLine(image_out, tuple(np.round(pos).astype(np.int32)),
+                    tuple(np.round([pos[0] + dx, pos[1] + dy]).astype(int)), color,
+                    thickness, cv2.LINE_AA, tipLength=0.18)
+    return image_out
